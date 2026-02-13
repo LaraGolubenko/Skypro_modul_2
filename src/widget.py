a976b614d817693b7fb5e8f7ac882fc4233a4c4b
Лариса Голубенко <golubenko.lara@gmail.com>
2026-02-13 22:41:51 +0500

    widget done

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,11 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+
 def mask_account_card(name_number: str) -> str:
     if "счет" in name_number.lower():
         return f"Счет {get_mask_account(name_number)}"
     else:
         return f"{name_number[:-16]} {get_mask_card_number(name_number[-16:])}"
+
+def get_date(date: str) -> str:
+    return date[8:10] + "." + date[5:7] + "." + date[:4]
